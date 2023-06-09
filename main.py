bf33d1b6901aa233a9583fb89fed5eab0600b560
MelGitPro <subaruphonk12@gmail.com>
2023-06-09 19:50:35 +0700

    up

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ app = Flask(__name__)
 def home():
     imagelist = os.listdir("./static/images/")
     print(imagelist)
-    return render_template('base.html', imagelist = imagelist)
+    return render_template('home.html', imagelist = imagelist)
 
+@app.errorhandler(404)
+def error_not_found(e):
+    return render_template("not-found.html") , 404
 
 
 if (__name__) == '__main__':
