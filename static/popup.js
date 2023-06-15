391bf4c55b2857b9e40947a31c39b150b58420c6
Meliodas-Git-0223 <meliodasrepl@gmail.com>
2023-06-15 21:08:54 +0700

    update 21:08

--- static/popup.js
+++ static/popup.js
@@ -1,12 +1,10 @@
 let popupBlock = document.querySelector(".popupBlock");
 let on = false;
 let popupImg = document.querySelector(".popupimage")
-
+/*
 function showPopup(block){
     let blockPicture = block.src;
     popupImg.src = blockPicture;
-    popupImg.style.maxWidth = "100%";
-    popupImg.style.maxHeigth = "100%";
     on = !on;
     if (on){
         popupBlock.style.cssText = "display:block;";
@@ -15,6 +13,44 @@ function showPopup(block){
     }
     
 }
+*/
+
+
+
+function showPopup(block) {
+    let blockPicture = block.src;
+    popupImg.src = blockPicture;
+    popupImg.style.maxWidth = "100%";
+
+    var img = new Image();
+    img.onload = function() {
+        var width = this.width;
+        var height = this.height;
+        console.log("Ширина изображения: " + width + " пикселей");
+        console.log("Высота изображения: " + height + " пикселей");
+
+        if (width < height){
+            popupBlock.style.width = "28%";
+            popupBlock.style.top = "80px";
+            popupBlock.style.right = "35%"
+        } else {
+            popupBlock.style.width = "50%";
+            popupBlock.style.top = "100px";
+        }
+    };
+    img.src = blockPicture;
+
+    
+
+    on = !on;
+    if (on) {
+        popupBlock.style.display = "block";
+    } else {
+        popupBlock.style.display = "none";
+    }
+}
+
+
 
 
 function closePopup(){
